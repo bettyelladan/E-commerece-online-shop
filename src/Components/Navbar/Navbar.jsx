68a4cea42e45b4ee4690e051327d4c1e83e7de55
bettyelladan <danieba@mail.uc.edu>
2024-10-07 15:57:36 -0400

    initail

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react' 6.9k (gzipped; 2.7k)
+import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+
+const Navbar = () => {
+    return (
+        <div classname='navbar'>
+
+        </div>
+    )
+}
+ 
+export default Navbar 
